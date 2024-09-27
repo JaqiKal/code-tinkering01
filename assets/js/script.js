1b583f7a9a3355436426b9065e4b9a8b60dac612
Jacqueline Kalmár <jaqika@gmail.com>
2024-09-27 12:18:01 +0000

    feat: Add styling updates and placeholders
    Add local font and color variables in CSS
    
    Update color scheme and add hover effects on links
    
    Add logo and placeholder text in Contact and About
    
    Add comments in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+/**
+ *  This script manages the toggle functionality of the mobile navigation menu.
+ *
+ * Features:
+ * - Toggles the visibility of the navigation menu on small screens.
+ * - Adds or removes the 'open' class on the navigation menu when the hamburger
+ *  icon is clicked.
+ *
+ * Other:
+ * - jshint esversion: 6, please see README.md for further details.
+ *
+ * Author: JaqiKal
+ * Date: Sept 2024
+ */
+
+// jshint esversion: 6
